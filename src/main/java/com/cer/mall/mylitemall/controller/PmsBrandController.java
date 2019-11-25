8841b32478e46bc6e8aa0e484d87d24a499cf63f
xujin <472829302@qq.com>
2019-11-25 17:02:26 +0800

    添加swagger2,修改mbg工具重新生成代码不覆盖报错的问题

--- src/main/java/com/cer/mall/mylitemall/controller/PmsBrandController.java
+++ src/main/java/com/cer/mall/mylitemall/controller/PmsBrandController.java
@@ -4,6 +4,8 @@ import com.cer.mall.mylitemall.common.CommonPage;
 import com.cer.mall.mylitemall.common.CommonResult;
 import com.cer.mall.mylitemall.mbg.model.PmsBrand;
 import com.cer.mall.mylitemall.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,7 @@ import java.util.List;
 /**
  *  品牌管理Controller
  */
+@Api(tags = "PmsBrandController", description = "商品品牌管理")
 @Controller
 @RequestMapping("/brand")
 public class PmsBrandController {
@@ -24,12 +27,14 @@ public class PmsBrandController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
 
+    @ApiOperation("获取所有品牌列表")
     @RequestMapping(value = "listAll", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<List<PmsBrand>> getBrandList() {
         return CommonResult.success(pmsBrandService.listAllBrand());
     }
 
+    @ApiOperation("添加品牌")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     @ResponseBody
     public CommonResult createBrand(@RequestBody PmsBrand pmsBrand) {
